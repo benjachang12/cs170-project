9698223f8d05588fcd2ede1f441c3ee2254ec964
benjachang12 <benjachang12@berkeley.edu>
2020-04-29 23:38:42 -0700

    tested write_output_files, restructured workspace

--- solver_ben.py
+++ solver_ben.py
@@ -1,11 +1,25 @@
 import networkx as nx
+import numpy as np
+import time
 from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance
 import sys
+import os
 
+
+
+"""
+CS170 Sp2020 Project: Cell Tower Network Design for Horizon Wireless
+
+
+authors: Benjamin Chang, Kelvin Pang 
+date: 4/29/2020
+"""
 
 def solve(G):
     """
+    Solves the problem statement
+
     Args:
         G: networkx.Graph
 
@@ -14,7 +28,12 @@ def solve(G):
     """
 
     # TODO: your code here!
-    pass
+    # build a spanning tree of n-vertex weighted graph
+    # TODO: implement algorithm from the paper
+
+
+    T = nx.maximum_spanning_tree(G)
+    return T
 
 
 # Here's an example of how to run your solver.
@@ -22,10 +41,29 @@ def solve(G):
 # Usage: python3 solver.py test.in
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 2
-    path = sys.argv[1]
-    G = read_input_file(path)
-    T = solve(G)
-    assert is_valid_network(G, T)
-    print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-    write_output_file(T, 'out/test.out')
+    # assert len(sys.argv) == 2
+    # path = sys.argv[1]
+    # G = read_input_file(path)
+    # T = solve(G)
+    # assert is_valid_network(G, T)
+    # print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+    # write_output_file(T, 'out/test.out')
+
+    output_dir = "experiment_outputs/test1"
+    input_dir = "inputs"
+    for input_path in os.listdir(input_dir):
+        graph_name = input_path.split(".")[0]
+        G = read_input_file(f"{input_dir}/{input_path}")
+
+        # Solve problem and time the elapsed time
+        start_time = time.time()
+        T = solve(G)
+        elapsed_time = time.time() - start_time
+
+        print("Finished solving:", graph_name)
+        print('With total runtime:', elapsed_time, "(s)")
+        print("With average  pairwise distance: {}".format(average_pairwise_distance(T)), "\n")
+
+        write_output_file(T, f"{output_dir}/{graph_name}.out")
+
+        # TODO: write a save_to_csv function that saves a table of inputs and their runtime
