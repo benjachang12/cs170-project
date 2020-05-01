d7be3a0cac4d32db20998bef5ba5442b40506831
Kelvin Pang <kelvinpang@berkeley.edu>
2020-04-30 17:25:46 -0700

    updated?

--- solver_kelvin.py
+++ solver_kelvin.py
@@ -1,8 +1,27 @@
 import networkx as nx
+import time
 from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance
 import sys
+import os
 
+"""
+CS170 Spring 2020 Project
+
+Kelvin Pang, Ben Chang
+
+"""
+
+"""
+Algorithm:
+
+Maxmimum Leaf Implementation
+
+- Get most leaves
+- Delete leaves
+- Have shortest tree with a set that approximately dominates graph
+
+"""
 
 def solve(G):
     """
@@ -14,7 +33,9 @@ def solve(G):
     """
 
     # TODO: your code here!
-    pass
+    T = nx.minimum_spanning_tree(G)
+    delete = [n for n in T if T.degree(n) == 1]
+    return T
 
 
 # Here's an example of how to run your solver.
@@ -22,10 +43,29 @@ def solve(G):
 # Usage: python3 solver.py test.in
 
 # if __name__ == '__main__':
-#     assert len(sys.argv) == 2
-#     path = sys.argv[1]
-#     G = read_input_file(path)
-#     T = solve(G)
-#     assert is_valid_network(G, T)
-#     print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-#     write_output_file(T, 'out/test.out')
+# #     assert len(sys.argv) == 2
+# #     path = sys.argv[1]
+# #     G = read_input_file(path)
+# #     T = solve(G)
+# #     assert is_valid_network(G, T)
+# #     print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+# #     write_output_file(T, 'out/test.out')
+
+
+if __name__ == "__main__":
+    output_dir = "experiment_outputs/test2"
+    input_dir = "inputs"
+    for input_path in os.listdir(input_dir):
+        graph_name = input_path.split(".")[0]
+        G = read_input_file(f"{input_dir}/{input_path}")
+
+        # Solve problem and time the elapsed time
+        start_time = time.time()
+        T = solve(G)
+        elapsed_time = time.time() - start_time
+
+        print("Finished solving:", graph_name)
+        print('With total runtime:', elapsed_time, "(s)")
+        print("With average  pairwise distance: {}".format(average_pairwise_distance(T)), "\n")
+
+        write_output_file(T, f"{output_dir}/{graph_name}.out")
