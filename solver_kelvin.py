bdaa69c93741c9991d34d14e668efac36a45b7fb
Kelvin Pang <kelvinpang@berkeley.edu>
2020-04-30 23:59:17 -0700

    resolved bugs in pruner

--- solver_kelvin.py
+++ solver_kelvin.py
@@ -4,6 +4,7 @@ from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance
 import sys
 import os
+import matplotlib.pyplot as plt
 
 """
 CS170 Spring 2020 Project
@@ -33,16 +34,36 @@ def solve(G):
     """
 
     # TODO: your code here!
-    T = nx.minimum_spanning_tree(G)
-    X = list(T.nodes)
+    M = nx.minimum_spanning_tree(G)
+    X = list(M.nodes)
     length = len(X)
-    U = T
-    for i in range(length):
-        a = X[i]
-        if T.degree[a] == 1:
-            U.remove_node(a)
-    T = U
+    U = M.copy()
+    if length > 2:
+        for i in range(length):
+            a = X[i]
+            if U.degree[a] < 2:
+                M.remove_node(a)
+    T = M
+    # visualize_results(G,T)
     return T
+
+def visualize_results(G, T):
+    """
+    Visualizes input graph and output tree side by side for easy comparison
+
+    :param G: input Graph
+    :param T: output Tree
+    :return:
+    """
+
+    plt.subplot(121)
+    nx.draw(G, with_labels=True)
+    plt.subplot(122)
+    nx.draw(T, with_labels=True)
+
+    plt.subplot(121).set_title('Graph')
+    plt.subplot(122).set_title('Tree')
+    plt.show()
 
 
 # Here's an example of how to run your solver.
@@ -66,3 +87,13 @@ if __name__ == "__main__":
         print("With average  pairwise distance: {}".format(average_pairwise_distance(T)), "\n")
 
         write_output_file(T, f"{output_dir}/{graph_name}.out")
+
+    # input_dir = "inputs"
+    # input_path = "medium-111.in"
+    #
+    #
+    # G = read_input_file(f"{input_dir}/{input_path}")
+    # T = solve(G)
+    # assert is_valid_network(G, T)
+    # print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+    # write_output_file(T, 'out/test.out')
